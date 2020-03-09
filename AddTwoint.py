5d906c9edba45e58f8e318b0878982316a9ed6ff
rindra-git <rindratsiorinantenaina@gmail.com>
2020-03-09 16:02:27 +0400

    ok

--- AddTwoint.py
+++ AddTwoint.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
 def add(a,b):
 	return a + b
 
 if __name__ == "__main__":
-	print(add(5,6))
+	if len(sys.argv)>3 :
+		print ("tu veux")
+	elif (len(sys.argv) == 3):
+		a = int( sys.argv[1] )
+		b = int( sys.argv[2] )
+		print((a),"+","=",(a+b))
+	elif len(sys.argv)==2:
+		print("tu")
+		print("veux: ")
+		a = int( sys.argv[1] )
+		b = int( input())
+		print((a),"+",(b),(a+b))
