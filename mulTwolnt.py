1a1621c689c7cc69e5353c335b7ea9089cc102b2
naudecle <naudecle@gmail.com>
2020-03-09 14:47:33 +0400

    fichier python

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+
 def mul(a,b):
 	return a * b
 
+
+
 if __name__ == "__main__":
-	print(mul(45,5))
+
+
+	if len(sys.argv) > 3 :
+    		print("Saisissez que deux arguments")
+
+	elif (len(sys.argv) == 3):
+		a = int(sys.argv[1])
+		b = int(sys.argv[2])
+		print (mul(a,b))
+
+	else:
+		print("Peu d'argument")
